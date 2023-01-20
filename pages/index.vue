586eb3513376741c19a60261d812580411ff792d
Ethan Liang <e3lcool@gmail.com>
2023-01-20 12:01:36 -0800

    PR rewording

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,14 @@
     <h1 class="text-center text-5xl sm:text-8xl text-transparent bg-gradient-to-br from-primary to-accent bg-clip-text font-bold">
         Gunn Elimination
     </h1>
-    <div class="mt-10 flex flex-col mx-auto text-center gap-8">
+    <div class="mt-10 flex flex-col mx-auto text-center gap-6 w-3/4">
+        <p class="">
+            Eliminate your target and evade your eliminator. Last one standing wins.
+        </p>
         <p>
-            <!-- TODO: rewrite this game info blurb to be shorter and more fun -->
-            At the beginning of the game (1/27/23, 12:00 A.M.), each player will be assigned another player (target). Your
-            mission is to eliminate your target by tagging them with a miniature stuffed plushie. After you eliminate your
-            first target, you will get assigned your target's target. Eliminations are only valid under certain circumstances
-            (see below). The game will end on 2/10/23 at 11:59 PM and whoever has the most kills by then wins!
+            The game will begin at midnight the morning of January 27th. Whoever has the most eliminations by 
+            the end of February 10th wins! Sign up by 1/25, and pick up plushies on 1/25 and 1/26 during lunch or afterschool on 
+            the senior quad. 
         </p>
         <a href="/app" class="block rounded bg-gradient-to-br from-primary to-accent shadow-lg shadow-primary/50 px-4 py-2 mx-auto text-white font-bold focus:outline-none focus-visible:ring-[3px]">
             Play Elimination
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,35 +1,16 @@
 <template>
-    <h1 class="title mb-8">Log In</h1>
+    <h1 class="title">Log In</h1>
     <p>
-        We don't sell your data because we don't know who to sell it to. Also,
-        please don't hack the Elimination website or any related service.
+        By continuing, you agree that you have read and will follow the Elimination rules and will
+        not attempt to exploit this website or any related service.
     </p>
-    <div class="flex-row flex mt-4">
-        <div class="form-control my-auto">
-            <label class="label gap-2 cursor-pointer">
-                <input
-                    v-model="agreed"
-                    type="checkbox"
-                    class="checkbox checkbox-sm checkbox-primary"
-                />
-                <span class="label-text">I understand</span>
-            </label>
-        </div>
-        <div v-if="agreed" class="my-auto ml-auto">
-            <client-only>
-                <a
-                    :href="`https://xz.ax/login?redirect_url=${protocol}//${host}/app`"
-                    class="btn btn-primary btn-sm font-bold"
-                >
-                    Log in
-                </a>
-            </client-only>
-        </div>
-    </div>
+    <a :href="`https://xz.ax/login?redirect_url=${protocol}//${host}/app`" class="btn btn-primary btn-sm font-bold w-1/4 h-10 my-4">
+        Continue
+    </a>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 
 const agreed = ref(false);
 const protocol = ref('');
