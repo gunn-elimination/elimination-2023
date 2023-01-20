113bd492d6d69cb0dfce25c5d1c723d3a6119f9c
Kevin Yu <kevinyu567@gmail.com>
2023-01-20 10:00:37 -0800

    Remove link to index from `GameHasNotBegun`

--- components/GameHasNotBegun.vue
+++ components/GameHasNotBegun.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="flex flex-col p-8 h-full items-center justify-center">
-        <h2 class="text-accent dark:text-primary text-center font-bold text-6xl mb-6">
+        <h2 class="text-accent dark:text-primary text-center font-bold text-6xl">
             The game has not begun.
         </h2>
-        <NuxtLink href="/" class="text-accent dark:text-primary">
-            Return to home.
-        </NuxtLink>
     </div>
 </template>
