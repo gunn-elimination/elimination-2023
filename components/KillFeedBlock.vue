5b98fcdadbf8ee724c6297501dd41ed8650375eb
Kevin Yu <kevinyu567@gmail.com>
2023-01-19 22:38:32 -0800

    Commit index page changes

---
+++ components/KillFeedBlock.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card bg-base-300 border border-gray-300/20">
+        <div class="card-body">
+            <h2 class="card-title text-lg">Alec Petridis eliminated Saumya Singhal</h2>
+            <p class="italic text-secondary">Yesterday at 3:51 PM</p>
+        </div>
+    </div>
+</template>
