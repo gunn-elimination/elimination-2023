2f883dfda38164eca4a283c3531fe3d67c96144a
Kevin Yu <kevinyu567@gmail.com>
2023-01-19 22:24:25 -0800

    Link rules to elim rules doc

--- components/HomeHeader.vue
+++ components/HomeHeader.vue
@@ -1,6 +1,8 @@
 <template>
     <nav class="flex gap-6 px-6 py-4 font-semibold">
-        <a href="/app" class="text-secondary">Rules</a>
+        <a href="https://tinyurl.com/gunnelim2023rules" target="_blank" rel="noopener noreferrer" class="text-secondary">
+            Rules
+        </a>
         <a href="/calendar" class="text-secondary">Calendar</a>
         <a href="/leaderboard" class="text-secondary">Leaderboard</a>
     </nav>
