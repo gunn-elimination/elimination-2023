0cd459f5f163f5ded37adac4785eb8f2b5f406d7
Kevin Yu <kevinyu567@gmail.com>
2023-01-20 00:06:10 -0800

    Add favicon, index link, use frontend routing

--- components/HomeHeader.vue
+++ components/HomeHeader.vue
@@ -1,9 +1,12 @@
 <template>
-    <nav class="flex gap-6 px-6 py-4 font-semibold">
+    <nav class="flex gap-6 px-6 py-4 font-semibold items-center">
+        <NuxtLink href="/">
+            <img src="/favicon.png" class="w-8 h-8" alt="Elimination logo" />
+        </NuxtLink>
         <a href="https://tinyurl.com/gunnelim2023rules" target="_blank" rel="noopener noreferrer" class="text-secondary">
             Rules
         </a>
-        <a href="/calendar" class="text-secondary">Calendar</a>
-        <a href="/leaderboard" class="text-secondary">Leaderboard</a>
+        <NuxtLink href="/calendar" class="text-secondary">Calendar</NuxtLink>
+        <NuxtLink href="/leaderboard" class="text-secondary">Leaderboard</NuxtLink>
     </nav>
 </template>
