6227e7a2214b2a217ee011966ef4f843ba88cb1b
Ethan Liang <e3lcool@gmail.com>
2023-01-20 12:01:36 -0800

    PR rewording

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="h-screen w-full">
-        <div class="container py-24">
+    <div class="h-screen w-full flex items-center">
+        <div class="container h-auto w-auto flex flex-col gap-6">
             <NuxtPage/>
         </div>
     </div>
