9f4aa86d2ba2dd0533540046394b431149f93f10
Kevin Yu <kevinyu567@gmail.com>
2023-01-21 10:26:13 -0800

    Rough initial calendar implementation
    See TODOs in `Calendar.vue`.

---
+++ components/CalendarCell.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        class="flex items-center justify-center p-6 2xl:p-8 relative border border-gray-300/10"
+        :class="!this.$slots.default && 'bg-base-200'"
+    >
+        <span class="absolute text-sm text-secondary top-2.5 left-3">
+            {{ date }}
+        </span>
+        <p class="text-sm 2xl:text-base" :class="bold && 'font-bold'">
+            <slot />
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CalendarCell",
+    props: {
+        date: {
+            type: String,
+            required: true
+        },
+        bold: Boolean
+    }
+}
+</script>
--- pages/app/calendar.vue
+++ pages/app/calendar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="h-full overflow-auto flex flex-col">
         <h1 class="title sticky-header p-8">Calendar</h1>
-        <GameHasNotBegun />
+        <div class="px-12 mt-6 pb-12">
+            <Calendar />
+        </div>
     </div>
 </template>
