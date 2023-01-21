ab0926fd0b631e2087a555b1b6080ad5d812baa1
Kevin Yu <kevinyu567@gmail.com>
2023-01-20 22:21:34 -0800

    Fix footer width

--- components/HomeFooter.vue
+++ components/HomeFooter.vue
@@ -1,8 +1,10 @@
 <template>
-    <footer class="container py-10 bg-base-200 text-secondary text-center text-sm">
-        <p>Made with love by SEC and student developers.</p>
-        <p>If you have questions or feedback, please contact <a href="mailto:kevinyu567@gmail.com" class="text-primary">kevinyu567@gmail.com</a>.</p>
-        <p>©2023</p>
+    <footer class="py-10 bg-base-200 text-secondary text-center text-sm">
+        <div class="container">
+            <p>Made with love by SEC and student developers.</p>
+            <p>If you have questions or feedback, please contact <a href="mailto:kevinyu567@gmail.com" class="text-primary">kevinyu567@gmail.com</a>.</p>
+            <p>© 2023</p>
+        </div>
     </footer>
 </template>
 
