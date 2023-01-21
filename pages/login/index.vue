edba8763824aa0941635178f13dc22918e494e5a
Ethan Liang <e3lcool@gmail.com>
2023-01-20 16:07:51 -0800

    revert yarn.lock and add request to use pausd email address

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -4,6 +4,9 @@
         By continuing, you agree that you have read and will follow the Elimination rules and will
         not attempt to exploit this website or any related service.
     </p>
+    <p>
+        Please log in using your pausd.us email address.
+    </p>
     <a :href="`https://xz.ax/login?redirect_url=${protocol}//${host}/app`" class="btn btn-primary btn-sm font-bold w-1/4 h-10 my-4">
         Continue
     </a>
