6bf09e82e120698d45e0394e985a490c31f57b91
Kevin Yu <kevinyu567@gmail.com>
2023-01-21 23:51:39 -0800

    Make sidebar display unread announcements hackily

--- components/GameSidebar.vue
+++ components/GameSidebar.vue
@@ -11,7 +11,12 @@
                         href="/app/announcements"
                     >
                         <div class="indicator">
-                            <span class="indicator-item badge badge-sm badge-error rounded-lg">2</span>
+                            <!-- TODO: `v-if` conditional render this if there's no unreads -->
+                            <span class="indicator-item badge badge-sm badge-error rounded-lg">
+                                <!-- TODO: this is very very ugly, but stores and reactivity somewhat force my hand. -->
+                                <!-- Think of a better way to do this perhaps -->
+                                {{ announcementsStore.announcements.filter((announcement) => announcement.active && currentTimeStore.time >= announcement.startDate && currentTimeStore.time <= announcement.endDate && !announcementsStore.readIds.includes(announcement.id)).length }}
+                            </span>
                             <MegaphoneIcon class="h-5 w-5 stroke-2"/>
                         </div>
                         Announcements
@@ -62,7 +67,7 @@
     </aside>
 </template>
 
-<script>
+<script lang="ts">
 import {HomeIcon, Cog6ToothIcon, MegaphoneIcon, CalendarIcon, TrophyIcon} from "@heroicons/vue/24/outline/index.js";
 
 export default {
@@ -74,4 +79,12 @@ export default {
         TrophyIcon,
     },
 };
+</script>
+
+<script setup lang="ts">
+import {useCurrentTimeStore} from '@/store/time';
+import {useAnnouncementsStore} from '@/store/announcements';
+
+const currentTimeStore = useCurrentTimeStore();
+const announcementsStore = useAnnouncementsStore();
 </script>
