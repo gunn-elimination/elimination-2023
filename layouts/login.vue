dc1e931247079b0049a02081f514e43b54f2c4e5
Ethan Liang <e3lcool@gmail.com>
2023-01-23 13:55:39 -0800

    Workaround message for login loop caused by blocked third-party cookies

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="h-screen w-full flex items-center">
-        <div class="container flex flex-col gap-6">
-            <NuxtPage/>
+        <div class="my-auto p-12">
+            <div class="flex flex-col gap-6">
+                <NuxtPage />
+            </div>
         </div>
     </div>
 </template>
