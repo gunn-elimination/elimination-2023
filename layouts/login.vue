feddf2b6fe5fe09b71f46514a048e06948aecd1d
Ethan Liang <e3lcool@gmail.com>
2023-01-23 14:32:50 -0800

    Styling changes to login page

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-screen w-full flex items-center">
-        <div class="my-auto p-12 flex flex-col gap-6">
+        <div class="w-full my-auto p-12 flex flex-col gap-6">
             <NuxtPage />
         </div>
     </div>
