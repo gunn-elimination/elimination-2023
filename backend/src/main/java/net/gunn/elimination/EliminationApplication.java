be4214a71a78bc33df648d971fd55c51182cf4dc
Alec Petridis <chop@quenda.net>
2023-01-24 13:23:42 -0800

    Make it use hte profile

--- backend/src/main/java/net/gunn/elimination/EliminationApplication.java
+++ backend/src/main/java/net/gunn/elimination/EliminationApplication.java
@@ -3,13 +3,29 @@ package net.gunn.elimination;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @SpringBootApplication
+@Configuration
 public class EliminationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EliminationApplication.class, args);
     }
+
+	@Bean
+	@Profile(value = "test")
+	public EmbeddedDatabase dataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setType(EmbeddedDatabaseType.H2)
+			.addScript("org/springframework/session/jdbc/schema-h2.sql").build();
+	}
 
 }
