e14609c8fda1725c97315be96f1c58bd378a1fd5
Kevin Yu <kevinyu567@gmail.com>
2023-01-25 08:46:32 -0800

    Add SSE for announcements

--- frontend/pages/app/announcements.vue
+++ frontend/pages/app/announcements.vue
@@ -2,10 +2,9 @@
     <div class="h-full overflow-auto flex flex-col">
         <h1 class="title sticky-header p-8">Announcements</h1>
 
-        <div v-for="announcement in store.announcements" :key="announcement.id" class="card w-full pt-6 pb-12 px-8">
+        <div v-for="announcement in store.filteredAnnouncements" :key="announcement.id" class="card w-full pt-6 pb-12 px-8">
             <!-- TODO: date formatting? -->
             <Announcement
-                v-if="announcement.active && currentTime.time >= announcement.startDate && currentTime.time <= announcement.endDate"
                 :title="announcement.title"
                 :body="announcement.body"
                 :start-date="DateTime.fromMillis(announcement.startDate).toLocaleString()"
@@ -17,9 +16,7 @@
 
 <script setup lang="ts">
 import {DateTime} from 'luxon';
-import {useCurrentTimeStore} from '@/store/time';
 import {useAnnouncementsStore} from '@/store/announcements';
 
-const currentTime = useCurrentTimeStore();
 const store = useAnnouncementsStore();
 </script>
