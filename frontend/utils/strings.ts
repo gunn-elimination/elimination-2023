2e531d570603f6518fcfb823fa7a0be9878e626d
Kevin Yu <kevinyu567@gmail.com>
2023-01-25 22:38:15 -0800

    Add first, second, third place to leaderboard

--- frontend/utils/strings.ts
+++ frontend/utils/strings.ts
@@ -1,3 +1,11 @@
 export function lowercaseFirstLetter(str: string) {
     return str.charAt(0).toLowerCase() + str.slice(1);
 }
+
+export function ordinal(x: number) {
+    const base = x % 10;
+    if (base === 1) return `${x}st`;
+    if (base === 2) return `${x}nd`;
+    if (base === 3) return `${x}rd`;
+    return `${x}th`;
+}
