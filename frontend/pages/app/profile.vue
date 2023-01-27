3d193ca6434956cd6db8d05351bf11fb959176d3
Kevin Yu <kevinyu567@gmail.com>
2023-01-26 23:29:00 -0800

    More profile work

--- frontend/pages/app/profile.vue
+++ frontend/pages/app/profile.vue
@@ -2,19 +2,35 @@
     <div class="h-full overflow-auto flex flex-col">
         <h1 class="title sticky-header p-8">Profile</h1>
         <div class="p-8 bg-secondary text-secondary-content">
-            <div class="container flex gap-4 sm:gap-8">
+            <div class="sm:container flex gap-6 sm:gap-8">
                 <Avatar
                     :user="currentUser"
                     large
                 />
 
                 <div class="flex flex-col">
-                    <h2 class="font-bold text-2xl mb-1">
+                    <h2 class="font-bold text-2xl">
                         {{ currentUser.forename }} {{ currentUser.surname }}
                     </h2>
-                    <span>{{ currentUser.email }}</span>
-                    <span><!-- #4 • -->{{ currentUser.eliminated.length }} eliminations</span>
+                    <span class="mb-2">{{ currentUser.email }}</span>
+
+                    <span><strong>Eliminated:</strong> {{ !!currentUser.eliminatedBy }}</span>
+                    <span v-if="currentUser.eliminatedBy">
+                        <strong>Eliminated by:</strong>
+                        <!-- TODO: fancier display -->
+                        {{ currentUser.eliminatedBy.forename }} {{ currentUser.eliminatedBy.surname }}
+                    </span>
+                    <span><strong>EER (elimination-eliminated-ratio):</strong> {{ currentUser.eliminated.length }}.0</span>
                 </div>
+            </div>
+        </div>
+        <div class="p-8">
+            <h3 class="text-lg font-semibold text-secondary">Your eliminations:</h3>
+            <p class="mt-6 text-secondary text-center italic" v-if="currentUser.eliminated.length === 0">
+                You have not eliminated anyone yet.
+            </p>
+            <div v-else>
+                <!-- TODO -->
             </div>
         </div>
     </div>
