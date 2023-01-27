e759db76393742a648cc16daaca5dd88e54958d4
Kevin Yu <kevinyu567@gmail.com>
2023-01-27 13:03:44 -0800

    Fix kill code modal on prod

--- frontend/components/KillCodeModal.vue
+++ frontend/components/KillCodeModal.vue
@@ -4,7 +4,7 @@
     <label for="code-modal" class="modal modal-middle bg-black/40">
         <label class="modal-box flex flex-col overflow-visible rounded-t-lg sm:rounded-lg" for="">
             <QrcodeVue
-                :value="`${config.public.apiUrl}/game/eliminate?code=${code}`"
+                :value="`https://elimination.gunn.one/api/game/eliminate?code=${code}`"
                 :size="256"
                 :margin="7"
                 class="absolute rounded-lg -top-4 left-0 right-0 mx-auto"
@@ -14,7 +14,7 @@
                 the elimination code shown below.
             </p>
             <code class="bg-base-200 rounded-lg p-5 font-mono text-secondary box-border">
-                {{ code }}
+                {{ store.code }}
             </code>
         </label>
     </label>
@@ -25,13 +25,12 @@ import QrcodeVue from 'qrcode.vue';
 
 export default {
     name: "KillCodeModal",
-    components: {QrcodeVue},
-    props: {
-        code: String
-    }
+    components: {QrcodeVue}
 }
 </script>
 
 <script setup lang="ts">
-const config = useRuntimeConfig();
+import {useUserStore} from '@/store/user';
+
+const store = useUserStore();
 </script>
