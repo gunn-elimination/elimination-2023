95572011970e3674e07e1aa84261599c5375bb98
Alec Petridis <chop@quenda.net>
2023-01-27 01:46:00 -0800

    Update sentry.client.ts

--- frontend/plugins/sentry.client.ts
+++ frontend/plugins/sentry.client.ts
@@ -6,7 +6,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     // const environment = nuxtApp.$config.NODE_ENV;
 
     Sentry.init({
-        dsn: 'https://10a99dc899b14b05a86a7177f92d68e6@o4504552325644288.ingest.sentry.io/4504552326823936',
+        dsn: 'https://a33dc5477849434fa1c892d37879138f@o4504552325644288.ingest.sentry.io/4504552326823937',
         environment: process.env.NODE_ENV,
         integrations: [new Integrations.BrowserTracing()],
         sampleRate: 1,
