7d18b53a8cc1b19541b6d9fc327d3a4899c9da5c
Alec Petridis <chop@quenda.net>
2023-01-27 13:13:02 -0800

    Improve error handling page

--- backend/src/main/java/net/gunn/elimination/routes/Error.java
+++ backend/src/main/java/net/gunn/elimination/routes/Error.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,34 +20,53 @@ import java.util.Map;
 
 @RestController
 public class Error implements ErrorController, AccessDeniedHandler {
-    private final ObjectMapper mapper;
+	private final ObjectMapper mapper;
 
-    public Error(ObjectMapper mapper) {
-        this.mapper = mapper;
-    }
+	public Error(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
 
-    @RequestMapping("/error")
-    public Object handleError(HttpServletRequest request) {
-        var code = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        code = code == null ? 500 : code;
-        var t = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+	@RequestMapping("/error")
+	public Object handleError(HttpServletRequest request) {
+		var code = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		code = code == null ? 500 : code;
+		var t = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
 
-        return Map.of("error", Map.of("code", code, "message", t != null ? ((Throwable) t).getMessage() : HttpStatus.resolve((Integer) code).getReasonPhrase()));
-    }
+		String errorCode = "Unknown Error";
+		if (t instanceof Throwable throwable) {
+			if (throwable.getMessage() != null)
+				errorCode = throwable.getMessage();
+			else
+				errorCode = throwable.getClass().getSimpleName();
+		} else if (code instanceof Integer i && i < 600) {
+			var status = HttpStatus.resolve(i);
+			if (status != null)
+				errorCode = status.getReasonPhrase();
+		}
 
-    @ExceptionHandler(NonPAUSDUserException.class)
-    public Object handleNonpausd(HttpServletRequest request, HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        return handleError(request);
-    }
 
-    @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendError(
-            HttpServletResponse.SC_FORBIDDEN,
-            mapper.writeValueAsString(Map.of("error", Map.of("code", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE), "message", accessDeniedException.getMessage())))
-        );
-    }
+		return Map.of(
+			"error",
+			Map.of(
+				"code", code,
+				"message", errorCode
+			)
+		);
+	}
+
+	@ExceptionHandler(NonPAUSDUserException.class)
+	public Object handleNonpausd(HttpServletRequest request, HttpServletResponse response) {
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		return handleError(request);
+	}
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+		response.sendError(
+			HttpServletResponse.SC_FORBIDDEN,
+			mapper.writeValueAsString(Map.of("error", Map.of("code", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE), "message", accessDeniedException.getMessage())))
+		);
+	}
 
 	@ExceptionHandler(RegistrationClosedException.class)
 	public void handleRegistrationClosed(HttpServletRequest request, HttpServletResponse response) throws IOException {
