f684f1f8b27416959349f7d6764b9b4cea188de4
Kevin Yu <kevinyu567@gmail.com>
2023-01-30 00:59:32 -0800

    Update kill feed SSE implementation

--- frontend/store/feed.ts
+++ frontend/store/feed.ts
@@ -1,12 +1,21 @@
 import { defineStore } from 'pinia'
 import {useEventSource} from '@vueuse/core';
+import type {EliminationFeedItem} from '@/utils/types';
 
 
 export const useKillFeedStore = defineStore('feed', () => {
     const config = useRuntimeConfig();
 
     const {data, error} = useEventSource(`${config.public.apiUrl}/game/eliminations`);
-    const feed = computed(() => data.value && JSON.parse(data.value));
+    const feed = ref<EliminationFeedItem[] | null>(null)
+    watch(data, (raw) => {
+        if (!raw) return;
+        const res = JSON.parse(raw);
+
+        // Handle both array and single-object events
+        if (res.type === 'bulkKillfeed') feed.value = [...feed.value ?? [], ...res.value];
+        else feed.value = [...feed.value ?? [], res.value];
+    })
 
     return {feed, error};
 });
