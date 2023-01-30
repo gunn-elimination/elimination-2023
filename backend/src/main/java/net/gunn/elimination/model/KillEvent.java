d176cd55906ca0f867b6f7d4dc3f35bfc8b7b4bb
Alec Petridis <chop@quenda.net>
2023-01-30 00:01:35 -0800

    fix typo

--- backend/src/main/java/net/gunn/elimination/model/KillEvent.java
+++ backend/src/main/java/net/gunn/elimination/model/KillEvent.java
@@ -3,13 +3,13 @@ package net.gunn.elimination.model;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "type")
 @JsonSubTypes(
 	{
 		@JsonSubTypes.Type(value = Kill.class, name = "Kill"),
 		@JsonSubTypes.Type(value = BulkKillfeed.class, name = "BulkKillfeed")
 	}
 )
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 public sealed interface KillEvent permits Kill, BulkKillfeed {
 
 }
