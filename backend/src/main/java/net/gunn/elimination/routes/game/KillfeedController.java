77fc187a43d084e712a65776345cc6180a2d19e1
Ethan Liang <e3lcool@gmail.com>
2023-01-29 23:17:09 -0800

    remove silly things from KillfeedController

--- backend/src/main/java/net/gunn/elimination/routes/game/KillfeedController.java
+++ backend/src/main/java/net/gunn/elimination/routes/game/KillfeedController.java
@@ -2,32 +2,24 @@ package net.gunn.elimination.routes.game;
 
 import io.sentry.spring.tracing.SentrySpan;
 import net.gunn.elimination.EliminationManager;
-import net.gunn.elimination.EmptyGameException;
-import net.gunn.elimination.IncorrectEliminationCodeException;
-import net.gunn.elimination.auth.EliminationAuthentication;
 import net.gunn.elimination.model.Kill;
 import net.gunn.elimination.repository.KillfeedRepository;
 import net.gunn.elimination.repository.UserRepository;
-import net.gunn.elimination.routes.SSEController;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManagerFactory;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/game")
-@PreAuthorize("@eliminationManager.gameIsOngoing() && hasRole('ROLE_PLAYER')")
+@PreAuthorize("@eliminationManager.gameIsOngoing()")
 @CrossOrigin(
-	origins = {"https://elimination-2023.vercel.app", "https://elimination.gunn.one", "http://localhost:3000"},
-	allowCredentials = "true",
-	exposedHeaders = "SESSION"
+	origins = {"https://elimination-2023.vercel.app", "https://elimination.gunn.one", "http://localhost:3000"}
 )
 public class KillfeedController {
 	public final EliminationManager eliminationManager;
@@ -44,9 +36,6 @@ public class KillfeedController {
 		this.emf = emf;
 	}
 
-	/**
-	 * @apiNote Gets the current user's elimination code.
-	 */
 	@GetMapping(value = "/eliminations", produces = "application/json")
 	@SentrySpan
 	@ResponseBody
