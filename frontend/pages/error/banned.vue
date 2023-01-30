1c895d10326174d646334e09285b905eca101e02
Kevin Yu <kevinyu567@gmail.com>
2023-01-29 22:51:50 -0800

    Add banned user page

---
+++ frontend/pages/error/banned.vue
@@ -0,0 +1,22 @@
+<template>
+    <Head>
+        <Title>Error: restricted account | Gunn Elimination</Title>
+    </Head>
+
+    <h1 class="title">Error: restricted account.</h1>
+    <div>
+        <p class="mb-2">
+            Your account has been restricted for violating the elimination rules. You have been eliminated and
+            will no longer be able to access the game.
+        </p>
+        <p class="mb-2">
+            This decision is final and no appeals can be made at this time.
+        </p>
+    </div>
+</template>
+
+<script>
+definePageMeta({
+    layout: "centered",
+});
+</script>
